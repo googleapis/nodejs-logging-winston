75897a989050b20f72bb5c5fd8feab1701c52c81
Michael Gao <fgao@google.com>
2017-11-16 14:47:01 -0800

    change CI config

--- system-test/logging-winston.js
+++ system-test/logging-winston.js
@@ -20,7 +20,7 @@ var assert = require('assert');
 var winston = require('winston');
 
 var logging = require('@google-cloud/logging')();
-var LoggingWinston = require('../build/src/').LoggingWinston;
+var LoggingWinston = require('../build/src/index.js').LoggingWinston;
 
 describe('LoggingWinston', function() {
   var WRITE_CONSISTENCY_DELAY_MS = 90000;
@@ -90,19 +90,21 @@ describe('LoggingWinston', function() {
         logger[test.level].apply(logger, test.args);
       });
       setTimeout(function() {
-        logging.log('winston_log').getEntries({
-          pageSize: testData.length,
-        },
-        function(err, entries) {
-          assert.ifError(err);
-          assert.strictEqual(entries.length, testData.length);
-          entries.reverse().forEach(function(entry, index) {
-            var test = testData[index];
-            test.verify(entry);
-          });
+        logging.log('winston_log').getEntries(
+          {
+            pageSize: testData.length,
+          },
+          function(err, entries) {
+            assert.ifError(err);
+            assert.strictEqual(entries.length, testData.length);
+            entries.reverse().forEach(function(entry, index) {
+              var test = testData[index];
+              test.verify(entry);
+            });
 
-          done();
-        });
+            done();
+          }
+        );
       }, WRITE_CONSISTENCY_DELAY_MS);
     });
   });
