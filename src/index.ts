1ed6a57a30067fe63b824f677d6e83917f3fc08d
Jean-Marc Arsan <arsan.jm@gmail.com>
2018-06-06 04:01:29 -0400

    fix:prevent permanent  merging of labels when adding custom labels to specific log

--- src/index.ts
+++ src/index.ts
@@ -170,7 +170,7 @@ export class LoggingWinston extends winston.Transport {
       // https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
       if ((metadata as types.Metadata).labels) {
         entryMetadata.labels = (entryMetadata.labels) ?
-            Object.assign(
+            Object.assign({},
                 entryMetadata.labels, (metadata as types.Metadata).labels) :
             (metadata as types.Metadata).labels;
         delete (data.metadata as types.Metadata).labels;
