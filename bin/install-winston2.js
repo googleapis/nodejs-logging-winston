d10f8cb43bd091fbddadd47bcc958e87ec5c1870
Ryan Day <ryaneday@google.com>
2018-09-11 14:13:57 -0700

    fix: moar windows ci fixes

--- bin/install-winston2.js
+++ bin/install-winston2.js
@@ -3,14 +3,17 @@ const fs = require('fs')
 const path = require('path')
 const spawn = require('child_process').spawn
 
-console.log(process.env)
 
 if(fs.existsSync(path.join(__dirname,'..','test','winston-2','node_modules','winston'))){
   process.exit(0)
 }
 
-let proc = spawn('npm',['install'],{cwd:path.join(__dirname,'..','test','winston-2'),stdio:'inherit'})
-
+let proc;
+if(process.env.npm_execpath){
+    proc = spawn(process.env.NODE||'node',[process.env.npm_execpath,'install'],{cwd:path.join(__dirname,'..','test','winston-2'),stdio:'inherit'})
+} else {
+    proc = spawn('npm',['install'],{cwd:path.join(__dirname,'..','test','winston-2'),stdio:'inherit'})
+}
 proc.on('close',(code)=>{
   process.exit(code||0)
 })
