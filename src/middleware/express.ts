98e70a32cd6fcc6e2713176394285b4868ef8510
Niklas Schlögel <niklasschloegel@gmail.com>
2024-02-02 12:36:14 +0100

    feat: propagate operations (#842)

--- src/middleware/express.ts
+++ src/middleware/express.ts
@@ -25,9 +25,12 @@ import {
   LOGGING_TRACE_KEY,
   LOGGING_SPAN_KEY,
   LOGGING_SAMPLED_KEY,
+  LOGGING_OPERATION_KEY,
 } from '../common';
 import {LoggingWinston, Options} from '../index';
 import {makeChildLogger} from './make-child-logger';
+import {google} from '@google-cloud/logging/build/protos/protos';
+import ILogEntryOperation = google.logging.v2.ILogEntryOperation;
 
 export const REQUEST_LOG_SUFFIX = '_reqlog';
 
@@ -100,7 +103,8 @@ export async function makeMiddleware(
       httpRequest: HttpRequest,
       trace: string,
       span?: string,
-      sampled?: boolean
+      sampled?: boolean,
+      operation?: ILogEntryOperation
     ) => {
       logger.info({
         // The request logs must have a log name distinct from the app logs
@@ -109,6 +113,7 @@ export async function makeMiddleware(
         [LOGGING_TRACE_KEY]: trace,
         [LOGGING_SPAN_KEY]: span,
         [LOGGING_SAMPLED_KEY]: sampled,
+        [LOGGING_OPERATION_KEY]: operation,
         httpRequest,
         message: httpRequest.requestUrl || 'http request',
       });
